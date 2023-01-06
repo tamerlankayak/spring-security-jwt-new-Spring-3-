798e9310cae7424806ed689961628a772c95361b
Tamerlan <tamerlan.mustafayev@rabita.az>
2023-01-06 15:23:13 +0400

    security first commit

---
+++ src/main/java/az/azintelecom/springsecuritynew/user/Role.java
@@ -0,0 +1,6 @@
+package az.azintelecom.springsecuritynew.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/az/azintelecom/springsecuritynew/SpringSecurityNewApplicationTests.java
@@ -0,0 +1,13 @@
+package az.azintelecom.springsecuritynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
